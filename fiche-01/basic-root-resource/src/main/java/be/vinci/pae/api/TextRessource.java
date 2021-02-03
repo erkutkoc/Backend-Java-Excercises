e89f657aa06ab37fab557d6430eff5feac6d88e9
erkutkoc <erkut.koc@outlook.com>
2021-02-03 11:35:51 +0100

    Fix - params

--- fiche-01/basic-root-resource/src/main/java/be/vinci/pae/api/TextRessource.java
+++ fiche-01/basic-root-resource/src/main/java/be/vinci/pae/api/TextRessource.java
@@ -3,10 +3,8 @@ package be.vinci.pae.api;
 import java.util.List;
 
 import be.vinci.pae.domain.Text;
-import be.vinci.pae.domain.Text.Level;
 import be.vinci.pae.services.DataServiceTextCollection;
 import jakarta.inject.Singleton;
-import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -19,7 +17,7 @@ public class TextRessource {
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Text> getAllText(@QueryParam("level") Level level) {
+	public List<Text> getAllText(@QueryParam("level") String level) {
 		if (level != null) {
 			return DataServiceTextCollection.getTexts(level);
 		}
